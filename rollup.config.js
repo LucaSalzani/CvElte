65436d194ce5b5671d380239612dc148ad803723
Luca Salzani <luca.salzani@zuehlke.com>
2024-03-24 00:40:20 +0100

    Update NPM Packages

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,9 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
+import css from 'rollup-plugin-css-only';
+import { spawn } from 'child_process'
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -16,13 +18,12 @@ export default {
 	},
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
-			}
+			compilerOptions: {
+				dev: !production
+			  }
+		}),
+		css({
+			output: 'bundle.css'
 		}),
 
 		// If you have external dependencies installed from
@@ -61,7 +62,7 @@ function serve() {
 			if (!started) {
 				started = true;
 
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+				spawn('npm', ['run', 'start', '--', '--dev'], {
 					stdio: ['ignore', 'inherit', 'inherit'],
 					shell: true
 				});
